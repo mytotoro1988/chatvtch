7bbe5e3f4bcf799b525bc99efec251b7e47947eb
logg <parkerjackson527@gmail.com>
2024-08-15 00:12:54 +0700

    add

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,58 +1,146 @@
-import { Email, LockOutlined, PersonOutline } from "@mui/icons-material";
+"use client";
+
+import {
+  EmailOutlined,
+  LockOutlined,
+  PersonOutline,
+} from "@mui/icons-material";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 const Form = ({ type }) => {
-
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm()
+  } = useForm();
+
+  const router = useRouter();
+
+  const onSubmit = async (data) => {
+    if (type === "register") {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      }
+
+      if (res.error) {
+        toast.error("Something went wrong");
+      }
+    }
+
+    if (type === "login") {
+      const res = await signIn("credentials", {
+        ...data,
+        redirect: false,
+      });
+
+      if (res.ok) {
+        router.push("/chats");
+      }
+
+      if (res.error) {
+        toast.error("Invalid email or password");
+      }
+    }
+  };
+
   return (
     <div className="auth">
       <div className="content">
         <img src="/assets/logo.png" alt="logo" className="logo" />
-  
-        <form className="form">
+
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
           {type === "register" && (
-            <div className="input">
-              <input
-              {...register("username"), {required: "Username is required"}}
-                type="text"
-                placeholder="Username"
-                className="input-field "
-              />
-              <PersonOutline sx={{ color: "#737373" }} />
+            <div>
+              <div className="input">
+                <input
+                  defaultValue=""
+                  {...register("username", {
+                    required: "Username is required",
+                    validate: (value) => {
+                      if (value.length < 3) {
+                        return "Username must be at least 3 characters";
+                      }
+                    },
+                  })}
+                  type="text"
+                  placeholder="Username"
+                  className="input-field"
+                />
+                <PersonOutline sx={{ color: "#737373" }} />
+              </div>
+              {errors.username && (
+                <p className="text-red-500">{errors.username.message}</p>
+              )}
             </div>
           )}
 
-          <div className="input">
-            <input type="email" placeholder="Email" className="input-field " />
-            <Email sx={{ color: "#737373" }} />
+          <div>
+            <div className="input">
+              <input
+                defaultValue=""
+                {...register("email", { required: "Email is required" })}
+                type="email"
+                placeholder="Email"
+                className="input-field"
+              />
+              <EmailOutlined sx={{ color: "#737373" }} />
+            </div>
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
           </div>
 
-          <div className="input">
-            <input
-              type="password"
-              placeholder="Password"
-              className="input-field "
-            />
-            <LockOutlined sx={{ color: "#737373" }} />
+          <div>
+            <div className="input">
+              <input
+                defaultValue=""
+                {...register("password", {
+                  required: "Password is required",
+                  validate: (value) => {
+                    if (
+                      value.length < 5 ||
+                      !value.match(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/)
+                    ) {
+                      return "Password must be at least 5 characters and contain at least one special character";
+                    }
+                  },
+                })}
+                type="password"
+                placeholder="Password"
+                className="input-field"
+              />
+              <LockOutlined sx={{ color: "#737373" }} />
+            </div>
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
           </div>
+
           <button className="button" type="submit">
-            {type == "register" ? "Join free" : "Let's chat"}
+            {type === "register" ? "Join Free" : "Let's Chat"}
           </button>
         </form>
+
         {type === "register" ? (
           <Link href="/" className="link">
             <p className="text-center">Already have an account? Sign In Here</p>
           </Link>
         ) : (
           <Link href="/register" className="link">
-            <p className="text-center">Don't have account ? Register Here</p>
+            <p className="text-center">Don't have an account? Register Here</p>
           </Link>
         )}
       </div>
